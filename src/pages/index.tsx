449cbe797de1768b82147e38d46bb2fad6ca2c28
xTidewaterx <79268288+xTidewaterx@users.noreply.github.com>
2023-03-27 15:51:25 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,50 +1,42 @@
+const value: string | number = "union type value, one of several types";
 
+import { GetStaticProps } from "next";
+import Link from "next/link";
 
-const value:  string | number = "union type value, one of several types";
-
-
-
-import { GetStaticProps } from 'next';
-import Link from 'next/link';
-
-function PresidentList( { presidents } : any){
-
-    console.log(presidents)
-    return( 
-        <div className='list__container p-5'>
-            <h1>Liste over Amerikanske presidenter 1817-1881</h1>
-            <div className='list__childContainer pt-5 align-self-lg-center'>
-            {
-                presidents.map( 
-                   (president : any ) =>
-                <div className='list__presidentContainer' key={president.id}>
-
-                    <Link style={{color: "black"}} href={ `presidents/${president.id}` } >
-                        <li className=' pb-2 list__presidentName'>{president.name}</li>
-                    </Link>
-                 </div>
-                 
-                 )
-            }
-        </div></div>
-    )
+function PresidentList({ presidents }: any) {
+  console.log(presidents);
+  return (
+    <div className="list__container p-5">
+      <h1>Liste over Amerikanske presidenter 1817-1881</h1>
+      <div className="list__childContainer pt-5 align-self-lg-center">
+        {presidents.map((president: any) => (
+          <div className="list__presidentContainer" key={president.id}>
+            <Link
+              style={{ color: "black" }}
+              href={`presidents/${president.id}`}
+            >
+              <li className=" pb-2 list__presidentName">{president.name}</li>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default PresidentList
- 
- 
+export default PresidentList;
+
 export const getStaticProps: GetStaticProps = async (context) => {
+  const response2 = await fetch(
+    "https://api.sampleapis.com/presidents/presidents"
+  );
 
-        const response2 = await fetch('https://api.sampleapis.com/presidents/presidents')
-    
-        const data = await response2.json();
-        
-        return {
-            props:{
-                presidents: data.slice(4,19),
-            },
-            
-        }
-}
+  const data = await response2.json();
 
+  return {
+    props: {
+      presidents: data.slice(4, 19),
+    },
+  };
+};
 
